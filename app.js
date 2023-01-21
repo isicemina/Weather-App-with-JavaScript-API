78692ca8aa5b71c6d60d0f48cb9327029322b20f
Emina <66750573+isicemina@users.noreply.github.com>
2023-01-21 19:27:06 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+let weather = {
+    "apiKey": "9f9de48668c947cb69bb50eedf9158a0",
+    fetchWeather: function (city) {
+        fetch(
+          "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric&appid=" +
+            this.apiKey
+        )
+        .then((response) => response.json())
+        .then ((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const {name} = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,humidity,speed)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+    },
+    search: function(){
+     this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click" , function(){
+weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(e){
+  if(e.key=="Enter"){
+    weather.search();
+  }
+});
+weather.fetchWeather("Tuzla");
